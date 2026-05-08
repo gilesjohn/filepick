90805783cd7e7d588542275789f9c30f3a7c63d2
giles <giles@holdsworth.live>
2026-05-08 19:54:36 +0100

    Adding file dialog and printing selected

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::{env, path::PathBuf};
+
+use rfd::FileDialog;
+
+// ## ADDING COMMITS RETROACTIVELY AS I DID IT ALL IN ONE GO, THESE ARE MUCH CLEANER THAN MY NORMAL INITIAL COMMITS 😅
+
 fn main() {
-    println!("Hello, world!");
+    let start_dir = env::args()
+        .nth(1)
+        .map(PathBuf::from)
+        .unwrap_or_else(|| ".".into());
+
+    let file_paths = FileDialog::new()
+        .set_directory(start_dir)
+        .pick_files()
+        .unwrap_or_default();
+
+    for file in file_paths {
+        println!("{}", file.display());
+    }
 }
